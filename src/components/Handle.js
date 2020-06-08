e9a6cf56572469d7e1c0c0d4473d353ddd44a45a
Jacob Kenyon <jacob.kenyon@distrelec.com>
2020-06-08 10:13:08 +0100

    project commit

---
+++ src/components/Handle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class handledata extends React.Component {
+
+    render() {
+        return(
+          <div>
+              Test
+          </div>
+        );
+    }
+
+}
+
+export default handledata;
